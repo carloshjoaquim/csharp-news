e7b81143e9db055c84957fb08707860b44d30a4c
Carlos Henrique <carloshj2011@gmail.com>
2018-06-12 18:01:20 -0300

    Incluindo projeto csharp6

---
+++ csharp 6/csharp 6/csharp 6/Classes/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_6.Classes
+{
+   public class Student
+    {
+        //Read-only properties
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        //Common properties
+        public int Age { get; set;}
+
+        //Auto-Property Initializers
+        public ICollection<double> Grades { get; } = new List<double>();
+
+        public Student(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+    }
+}
---
+++ csharp 6/csharp 6/csharp 6/Program.cs
@@ -0,0 +1,30 @@
+﻿using csharp_6.Classes;
+using System;
+using static System.String;
+
+namespace csharp_6
+{
+   public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student student = new Student("Carlos", "Joaquim");
+            Console.WriteLine(student.FirstName + " - First Name");
+            Console.WriteLine(student.LastName + " - Last Name");
+
+            student.Age = 25;
+            student.Grades.Add(8);
+            student.Grades.Add(6.4);
+
+            Console.WriteLine(student.Age + " - Age Name");
+            foreach (double g in student.Grades)
+            {
+                Console.WriteLine("Grade: " + g.ToString());
+            }
+
+            //
+
+            Console.ReadKey();
+        }
+    }
+}
