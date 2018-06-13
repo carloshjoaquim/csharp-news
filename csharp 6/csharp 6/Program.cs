cbe08c3641d8795e951b7a7fdf9fdce50135e24b
Carlos Henrique <carloshj2011@gmail.com>
2018-06-13 18:48:06 -0300

    Filtro de exceção

--- csharp 6/csharp 6/Program.cs
+++ csharp 6/csharp 6/Program.cs
@@ -1,12 +1,14 @@
 ﻿using csharp_6.Classes;
 using System;
+using System.Globalization;
 using System.Linq;
-using static System.String;
+using System.Threading;
 using static System.Math;
+using static System.String;
 
 namespace csharp_6
 {
-   public static class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -43,6 +45,17 @@ namespace csharp_6
 
             //string interpolation
             Console.WriteLine("Average of Grades: {0} - Squared Average: {1}", average, squared_average );
+
+            //Exception Filters
+            try
+            {
+                var grade = student.Grades.ElementAt(10000);
+                Console.WriteLine(grade);
+            }
+            catch(ArgumentOutOfRangeException ex) when (ex.StackTrace.Contains("OutOfRange"))
+            {
+                Console.WriteLine("Exceção filtrada - Índice inválido");
+            }
 
 
             Console.ReadKey();
