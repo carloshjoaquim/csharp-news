03c9553d8fc3fd0bbae415dfeb9d14f5b2833a8d
Carlos Henrique <carloshj2011@gmail.com>
2018-06-24 15:01:13 -0300

    Incluindo csharp 7

---
+++ csharp 7/csharp 7/Classes/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.String;
+
+namespace csharp_6.Classes
+{
+    public class Student
+    {
+        //Read-only properties
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        //Common properties
+        public int Age { get; set;}
+        public string Class { get; set; }
+
+        //Auto-Property Initializers
+        public ICollection<string> Friends { get; } = new List<string>();
+
+        //Expression-bodied 
+        public string FullName => $"{FirstName} {LastName}";
+
+        //Index Initializers
+        public Dictionary<string, double> MGrades = new Dictionary<string, double>
+        {
+            ["English"] = 6.5,
+            ["Math"] = 9.9,
+            ["Geography"] = 8.5,
+            ["History"] = 5.5,
+        };
+
+        public Student(string firstName, string lastName)
+        {
+            //Expressions
+            if (IsNullOrWhiteSpace(lastName))
+                Console.WriteLine("Parameter cannot be blank: {0}", nameof(lastName));
+
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public bool Passed(string grade, out double average)
+        {
+            average = MGrades.Where(x => x.Key.Equals(grade)).FirstOrDefault().Value;
+            // Returns true if grade is bigger or equal 6
+            return MGrades.Where(x => x.Key.Equals(grade)).FirstOrDefault().Value >= 6;
+        }
+
+        public (string matter, double note) BestMatter()
+        {
+            var result = MGrades.OrderByDescending(x => x.Value).FirstOrDefault();
+            return (result.Key, result.Value);
+        }
+
+        public (string matter, double note) WorstMatter()
+        {
+            var result = MGrades.OrderByDescending(x => x.Value).LastOrDefault();
+            return (result.Key, result.Value);
+        }
+
+        public Student() { }
+
+    }
+}
---
+++ csharp 7/csharp 7/Program.cs
@@ -0,0 +1,83 @@
+﻿using csharp_6.Classes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using static System.Math;
+using static System.String;
+
+namespace csharp_6
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student student = new Student("Carlos", "");
+            Student student2 = new Student();
+
+            //string interpolation + null - conditional operators
+            Console.WriteLine("First Name: {0}" , student?.FirstName);
+            Console.WriteLine("Last Name: {0}", student?.LastName);
+            Console.WriteLine("FullName : {0}", student?.FullName);
+
+            student.Age = 25;
+            student.Friends.Add("José");
+            student.Friends.Add("Marina");
+
+            //string interpolation 
+            Console.WriteLine("Age: {0}" , student.Age);
+            foreach (KeyValuePair<string, double> g in student.MGrades)
+            {
+                //string interpolation
+                Console.WriteLine("{0} : {1}", g.Key , g.Value);
+            }
+
+            //using static for String
+            if (IsNullOrEmpty(student.Class))
+                student.Class = "2nd - A";
+
+            //using static for Math
+            var average = student.MGrades.Values.Average();
+            var squared_average = Pow(average, 2);
+
+            //null - conditional operators
+            Console.WriteLine("Class: {0}", student?.Class);
+
+            //string interpolation
+            Console.WriteLine("Average of Grades: {0} - Squared Average: {1}", average, squared_average );
+
+            //Exception Filters
+            try
+            {
+                var grade = student.Friends.ElementAt(10000);
+                Console.WriteLine(grade);
+            }
+            catch(ArgumentOutOfRangeException ex) when (ex.StackTrace.Contains("OutOfRange"))
+            {
+                Console.WriteLine("Filtered Exception- Invalid Index for grades list");
+            }
+
+            // out variables
+            if (student.Passed("History", out double note))
+                Console.WriteLine("Passed ! The note is: {0}", note);
+            else
+            {
+                Console.WriteLine("Not Passed in History! The note is: {0}", note);
+            }
+
+            // tuple
+            (string matter, double vgrade) = student.BestMatter();
+            Console.WriteLine("Your best Matter is {0} and your grade is : {1}", matter, vgrade);
+
+            // discards
+            (string worst, _) = student.WorstMatter();
+            Console.WriteLine("Your worst Matter is: {0} ", worst);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
