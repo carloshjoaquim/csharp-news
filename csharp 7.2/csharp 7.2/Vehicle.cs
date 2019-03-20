ecf1995efb298fdae9202810df5e9be63fcd4554
Carlos Joaquim <carlos.joaquim@dextra-sw.com>
2019-03-20 13:49:19 -0300

    Incluindo csharp 7.2

---
+++ csharp 7.2/csharp 7.2/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace csharp_7._2
+{
+    public class Vehicle
+    {
+        public string Color { get; set; }
+        public int CV { get; set; }
+        public string Code { get; set; }
+
+
+        private protected void ShowProperties()
+        {
+            Console.WriteLine($"Type: {Color}-{CV}-{Code}");
+        }
+    }
+}
